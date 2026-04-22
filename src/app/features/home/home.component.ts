df2a63b32e6b045d3018ead95599241a91e0581c
rocio <rociolamcab@gmail.com>
2026-04-22 12:29:34 +0200

    feat: rediseño de la home con bootstrap y variables personalizadas

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,22 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from '../../shared/components/search-bar/search-bar.component';
-import { CourseCardComponent } from '../../shared/components/course-card/course-card.component';
-import { JobCardComponent } from '../../shared/components/job-card/job-card.component';
-import { CourseService } from '../../core/services/course.service';
-import { JobService } from '../../core/services/job.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SearchBarComponent, CourseCardComponent, JobCardComponent],
+  imports: [CommonModule, SearchBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  private courseService = inject(CourseService);
-  private jobService = inject(JobService); 
-
-  featuredCourses = this.courseService.getFeaturedCourses();
-  featuredJobs = this.jobService.getFeaturedJobs(); 
 }
